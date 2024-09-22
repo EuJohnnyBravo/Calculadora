520805b4672287e95e61bb0664a0daa378bad2a7
Rodrigo Oliveira <rodrigo.silva.oliveira369@gmail.com>
2024-09-21 22:44:09 -0300

    funcao divisao

--- script.js
+++ script.js
@@ -20,3 +20,12 @@ function multiplicacao() {
   let { numero1, numero2 } = pegaValores();
   resultado.innerHTML = numero1 * numero2;
 }
+
+function divisao() {
+  let { numero1, numero2 } = pegaValores();
+  if (numero2 === 0) {
+    resultado.innerHTML = "O segundo valor precisa ser diferente de 0";
+  } else {
+    resultado.innerHTML = numero1 / numero2;
+  }
+}
